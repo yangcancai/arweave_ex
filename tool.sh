74d81580b4b20180637ec0c8db7bbbcc06ceab84
Cam <cam@CamdeMacBook-Pro.local>
2023-06-08 11:07:07 +0800

    Shell script compatibility with Ubuntu systems

--- tool.sh
+++ tool.sh
@@ -3,18 +3,18 @@
 # rebar 3.16.1 on Erlang/OTP 24 Erts 12.2.1,IEx 1.14.3,Mix 1.14.3
 # ar-2.6.7.1
 
-if [ "$#" -gt 0 ] && [ `uname -s` == "Darwin" ]; then
+if [ "$#" -gt 0 ] && [ `uname -s`=="Darwin" ]; then
     RANDOMX_JIT="disable randomx_jit"
 else
     RANDOMX_JIT=
 fi
-cp rebar.config arweave/apps/arweave/
-cp arweave.app.src arweave/apps/arweave/src
 setup(){
 git submodule update --init --recursive
 # 1. Fix mac df shell
 # 2. replace rebar.config.script
 # 3. mix compile
+cp rebar.config arweave/apps/arweave/
+cp arweave.app.src arweave/apps/arweave/src
 mix deps.get
 mkdir -p data
 cp -r arweave/data/* data/
@@ -34,7 +34,7 @@ mainnet(){
   iex --erl "+MBas aobf +MBlmbcs 512 +Ktrue +A20 +SDio20 +sbwtvery_long +sbwtdcpuvery_long +sbwtdiovery_long +swtvery_low +swtdcpuvery_low +swtdiovery_low " -S mix phx.server
 }
 build(){
-  if [ "$1" == "mainnet" ]; then
+  if [ "$1"=="mainnet" ]; then
     cp rebar.config.script_mainnet arweave/apps/arweave/rebar.config.script
   else
    cp rebar.config.script_testnet arweave/apps/arweave/rebar.config.script
