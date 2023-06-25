9ea43a6803f6201aa0056c8f4618e8c735767830
Cam <cam@CamdeMacBook-Pro.local>
2023-06-25 18:24:12 +0800

    add run.sh script

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export DATABASE_URL=""
+export SECRET_KEY_BASE=""
+export AR_ARGS="$RANDOMX_JIT peer 188.166.200.45 peer 188.166.192.169 peer 163.47.11.64 peer 139.59.51.59 peer 138.197.232.192"
+bin/arweave_ex start_iex
